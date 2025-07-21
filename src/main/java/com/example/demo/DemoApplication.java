d1b915ddc6257379b4dfe6307feaa52143e7c75c
Josef Kopriva <jkopriva@redhat.com>
2025-07-21 17:00:00 +0200

    Test commit from TSSC e2e test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World - Updated! 1753109986726 !";
     }
 
     public static void main(String[] args) {
